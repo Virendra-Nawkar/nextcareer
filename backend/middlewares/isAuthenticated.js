89f5f0010cda069bc4bb0dd0d67ea25b476d6a7c
Virendra Nawkar <virendranawkar1@gmail.com>
2025-06-04 20:25:44 +0530

    PDF direct downlaod error

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -10,15 +10,17 @@ const isAuthenticated = async (req, res, next) => {
             })
         }
         const decode = await jwt.verify(token, process.env.SECRET_KEY);
-        if(!decode){
+        if (!decode) {
             return res.status(401).json({
-                message:"Invalid token",
-                success:false
+                message: "Invalid token",
+                success: false
             })
         };
         //  console.log("Decoded token:", decode);
 
-        req.id = decode.userId;
+        // req.id = decode.userId;
+        req.user = { id: decode.userId };
+
         next();
     } catch (error) {
         console.log(error);
